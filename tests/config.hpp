b96f3b31c0f87593b810b74f6db9bd9d51f6bb0e
Alena Egorova <CatInCosmicSpace@users.noreply.github.com>
2016-12-18 14:39:42 +0300

    Changed user settings

--- tests/config.hpp
+++ tests/config.hpp
@@ -27,18 +27,18 @@
 static std::map<std::string, std::string> options_with_proxy = 
 { 
 	{ "webdav_hostname", 	"https://webdav.yandex.ru" },
-	{ "webdav_login", 		"webdav.test.login" },
-	{ "webdav_password", 	"webdav.test.password" },
+	{ "webdav_login", 	"webdav.test.travis" },
+	{ "webdav_password", 	"webdav.test.test" },
 	{ "proxy_hostname", 	"{proxy_hostname}" },
-	{ "proxy_login", 		"{proxy_login}" },
+	{ "proxy_login", 	"{proxy_login}" },
 	{ "proxy_password", 	"{proxy_password}" }
 };
 
 static std::map<std::string, std::string> options_without_proxy =
 {
 	{ "webdav_hostname", 	"https://webdav.yandex.ru" },
-	{ "webdav_login", 	"webdav.test.login" },
-	{ "webdav_password", 	"webdav.test.password" }
+	{ "webdav_login", 	"webdav.test.travis" },
+	{ "webdav_password", 	"webdav.test.test" }
 };
 
 #ifdef WITH_PROXY
