9c4c91fbafceb1017b4623444e5b3296cd5165ac
Alena Egorova <CatInCosmicSpace@users.noreply.github.com>
2016-12-18 20:50:36 +0300

    Update client_test.cpp

--- tests/client_test.cpp
+++ tests/client_test.cpp
@@ -69,7 +69,7 @@ SCENARIO("Uploading", "upload") {
 	WebDAV::LocalClient::set_options("webdav.test.travis", "webdav.test.test", options);
 	std::unique_ptr<WebDAV::Client> client(WebDAV::Client::Init(options));
 
-	WebDAV::LocalClient::upload("test/", "/tmp_dir/", client);
+	WebDAV::LocalClient::upload("", "/tmp_dir/", client);
 
 	WebDAV::LocalClient::clear_encrypted("test/");
 	WebDAV::LocalClient::clear_hashes("test/");
