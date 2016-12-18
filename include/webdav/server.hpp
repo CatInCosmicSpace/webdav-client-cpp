a4918f2076ae0d9e052357ca46688f4bfc347c7f
Alena Egorova <CatInCosmicSpace@users.noreply.github.com>
2016-12-18 22:18:45 +0300

    Update server.hpp

--- include/webdav/server.hpp
+++ include/webdav/server.hpp
@@ -13,7 +13,7 @@ namespace WebDAV {
 		auto check_not_sha(std::vector<std::string> &) -> std::vector<std::string>;
 		auto check_sha(std::vector<std::string> &) -> std::vector<std::string>;
 		auto decrypt(std::string & name) -> const std::string;
-		auto decrypt_and_clear(std::string & path) -> void;
+		auto decrypt_and_clear(std::string path) -> void;
 		auto download(const std::string & dir, const std::string & down_dir, std::unique_ptr<WebDAV::Client> & client) -> void;
 		auto file_list(const std::string & path)->std::vector<std::string>;
 		auto info_to_string(const std::map<std::string, std::string> & info) -> const std::string;
