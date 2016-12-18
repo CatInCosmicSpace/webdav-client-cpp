e2b5cf4c313da763f845c158ce1920cc5c8423b1
Alena Egorova <CatInCosmicSpace@users.noreply.github.com>
2016-12-18 17:36:26 +0300

    Update input.cpp

--- tests/input.cpp
+++ tests/input.cpp
@@ -1,8 +1,11 @@
 #include <fstream>
+#include <boost/filesystem.hpp>
 #include "catch.hpp"
+#include <iostream>
 
 SCENARIO("Open file") {
 	std::ifstream test("test.txt");
-	
+	std::cout << boost::filesystem::current_path();
+
 	REQUIRE(test.is_open());
 }
